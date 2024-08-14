885072bd62d31c7a6469669be4a5cf21e814e9dc
Nick Hellemans <nick@awellhealth.com>
2024-08-14 12:55:34 +0200

    chore(): bump versioné

---
+++ src/hostedPages/index.tsx
@@ -0,0 +1,1 @@
+export { SchedulingActivity } from './SchedulingActivity';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './atoms';
 export * from './molecules';
+export * from './hostedPages';
 export * from './lib/api';
