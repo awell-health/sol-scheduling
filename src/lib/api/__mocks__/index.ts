e2271c0fc1beb307dabf7ef6e84ca6a6a2e112d9
Nick Hellemans <nick@awellhealth.com>
2024-09-27 10:31:59 +0100

    chore(): add test

--- src/lib/api/__mocks__/index.ts
+++ src/lib/api/__mocks__/index.ts
@@ -1,7 +1,7 @@
 import { GetProvidersInputType } from '../schema';
-import { mockProvidersResponse } from './providers.mock';
-import { mockProviderAvailabilityResponse } from './providerAvailability.mock';
-import { mockProviderResponse } from './provider.mock';
+import { mockProvidersResponse } from './GetProviders.mock';
+import { mockProviderAvailabilityResponse } from './GetProviderAvailability.mock';
+import { mockProviderResponse } from './GetProvider.mock';
 
 const mockFetchProvidersFn = (_prefs: GetProvidersInputType) => {
   console.log('mock filter by', _prefs);
