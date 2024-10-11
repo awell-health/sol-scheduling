efe0889d202a97ca0bd9bc68143bdbf5615bcb9c
JB <jonathan@awellhealth.com>
2024-10-11 09:10:19 -0700

    update 'both' to 'In-Person' for availability location

--- src/lib/api/schema/atoms/EventDeliveryMethod.schema.ts
+++ src/lib/api/schema/atoms/EventDeliveryMethod.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export enum EventDeliveryMethod {
   Telehealth = 'Telehealth',
-  Both = 'both'
+  InPerson = 'In-Person'
 }
 
 export const EventDeliveryMethodSchema = z.nativeEnum(EventDeliveryMethod);
