918af78e35c77049cf32873996dbce66a0ad4bf4
Abigaila Ekiert <44117299+sharlotta93@users.noreply.github.com>
2024-11-18 11:16:48 +0100

    update-avatar-size (#73)

--- src/atoms/ProviderSelection/ProviderCard.tsx
+++ src/atoms/ProviderSelection/ProviderCard.tsx
@@ -40,7 +40,7 @@ export const ProviderCard: FC<ProviderProps> = ({
           <ProviderAvatar
             name={providerName}
             image={provider.image}
-            classes='w-24 h-24 '
+            classes='w-32 h-32'
           />
           <div className='flex flex-col justify-center'>
             <h3 className='text-slate-800 text-lg m-0 font-semibold'>
