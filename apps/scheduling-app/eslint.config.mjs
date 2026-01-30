7ff0a7cc7e4a2b7bfd06ce72ef98c9d2f80acc6d
JB <jonathan@awellhealth.com>
2026-01-30 13:59:12 -0500

    chore: exclude test and story files from build
    - Exclude *.stories.tsx, *.test.ts, *.test.tsx from tsconfig
    - Relax unused-vars ESLint rule for test and story files

--- apps/scheduling-app/eslint.config.mjs
+++ apps/scheduling-app/eslint.config.mjs
@@ -4,9 +4,15 @@ import storybook from "eslint-plugin-storybook";
 import config from '@repo/eslint-config/next';
 
 export default [...config, {
-  files: ['**/*.spec.ts', '**/__checks__/**/*.ts'],
+  files: ['**/*.spec.ts', '**/__checks__/**/*.ts', '**/*.test.ts', '**/*.test.tsx', '**/__tests__/**/*.ts'],
   rules: {
     'no-empty-pattern': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+}, {
+  files: ['**/*.stories.tsx'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }, ...storybook.configs["flat/recommended"]];
 
